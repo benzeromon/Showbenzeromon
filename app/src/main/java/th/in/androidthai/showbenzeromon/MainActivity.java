628e6d1366366049eac8bad3f30dda3635a46152
benzeromon <warinpiphat.wp@bru.ac.th>
2018-04-25 14:08:50 +0700

    Initial commit

---
+++ app/src/main/java/th/in/androidthai/showbenzeromon/MainActivity.java
@@ -0,0 +1,22 @@
+package th.in.androidthai.showbenzeromon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import th.in.androidthai.showbenzeromon.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+// Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+            .commit();
+
+        }
+    }  // Main Method
+}   // Main Class
---
+++ app/src/main/java/th/in/androidthai/showbenzeromon/fragment/MainFragment.java
@@ -0,0 +1,22 @@
+package th.in.androidthai.showbenzeromon.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import th.in.androidthai.showbenzeromon.R;
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+
+    }
+}   //Main Class
