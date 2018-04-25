a11b1b59391e84a71e8be727b6d7c6f3c8059139
benzeromon <warinpiphat.wp@bru.ac.th>
2018-04-25 16:28:28 +0700

    Replace Fragment Success

--- app/src/main/java/th/in/androidthai/showbenzeromon/fragment/MainFragment.java
+++ app/src/main/java/th/in/androidthai/showbenzeromon/fragment/MainFragment.java
@@ -7,10 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import th.in.androidthai.showbenzeromon.R;
 
 public class MainFragment extends Fragment{
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+     //   regiter Contrrller
+
+
+        TextView textView  = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+
+
+
+
+            }
+        });
+
+    }
 
     @Nullable
     @Override
